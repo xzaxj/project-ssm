d6a6c40609ebd8ba37ed116da6071ebf91d88170
zyb <smile@zyb.com>
2022-08-19 19:41:27 +0800

    ssm

--- project_ssm_template/src/main/java/com/zyb/ssm/controller/Test.java
+++ project_ssm_template/src/main/java/com/zyb/ssm/controller/Test.java
@@ -3,6 +3,6 @@ package com.zyb.ssm.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("test2");
+        System.out.println("testhot");
     }
 }
